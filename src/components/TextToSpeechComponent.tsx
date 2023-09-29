91d91a4e1289b41150688f223a0d7c2174c853a0
ArminasAcas <arminasprollas@gmail.com>
2023-09-29 12:54:24 +0300

    added extra functionality

--- src/components/TextToSpeechComponent.tsx
+++ src/components/TextToSpeechComponent.tsx
@@ -5,12 +5,17 @@ export default function TextToSpeech() {
 
     const [gender, setGender] = useState("Male");
     const [text, setText] = useState("");
+    const [language, setLanguage] = useState("English");
+    const [warning, setWarning] = useState(false);
 
-    function handleClick() {
-        if(text) 
+    function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        e.preventDefault();
+
+        if(text && text.length > 0) 
         {
             const FormData = {
                 text: text,
+                language: language,
                 gender: gender
             };
     
@@ -23,24 +28,41 @@ export default function TextToSpeech() {
                 body: JSON.stringify({FormData}),
             })
             .then((res) => res.json ());
+
+            if(warning) setWarning(false);
         }
+        else if (!warning) setWarning(true);
     };
 
     return (
         <>
         <form className="generation-form">
             <div className="generation-form__options">
-                <label htmlFor="gender">Generuojamo balso lytis</label>
-                <select onChange={e => {setGender(e.target.value)}} className="generation-form__select" id="gender">
-                    <option value="MALE">Vyras</option>
-                    <option value="FEMALE">Moteris</option>
-                    <option value="NEUTRAL">Neutralus</option>
+                <label htmlFor="language">Balso kalba</label>
+                <select onChange={e => {setLanguage(e.target.value)}} className="generation-form__select" id="language">
+                    <option value="English">Anglų</option>
+                    <option value="Lithuanian">Lietuvių</option>
                 </select>
+
+                {language === "English" ?
+                (
+                    <>
+                        <label htmlFor="gender">Balso lytis</label>
+                        <select onChange={e => {setGender(e.target.value)}} className="generation-form__select" id="gender">
+                        <option value="MALE">Vyras</option>
+                        <option value="FEMALE">Moteris</option>
+                        </select>
+                    </>
+                ) : null}
+                
             </div>
 
             <div className="generation-form__text-block">
-                <label htmlFor="text">Generuojamas tekstas</label>
-                <textarea onChange={e => {setText(e.target.value)}} className="generation-form__text-area" id="text"></textarea>
+                <div>
+                    <label htmlFor="text">Tekstas</label>
+                    {warning ? <span className="generation-form__warning"> [Tusčias teksto laukas !]</span> : null}
+                </div>
+                <textarea onChange={e => {setText(e.target.value)}} maxLength={300} className="generation-form__text-area" id="text"></textarea>
                 <button className="generation-form__button" onClick={handleClick} >Generuoti balso takelį</button>
             </div>
         </form>
